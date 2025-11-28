0fe0dc1fffad99f0c8b568aed7b0c72e0dbc39af
mac0923 <aerymzc9123@gmail.com>
2025-11-28 11:42:47 +0800

    feat: add wagmi

---
+++ src/components/WalletInfo.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { useConnection, useDisconnect } from "@wagmi/vue";
+
+const { address, connector } = useConnection();
+const { disconnect } = useDisconnect();
+
+const t = async () => {
+  const p = await connector.value?.getProvider();
+};
+</script>
+
+<template>
+  <div>
+    <div>Address: {{ address }}</div>
+    <div>Connected to {{ connector?.name }} Connector.</div>
+    <button @click="disconnect()">Disconnect</button>
+  </div>
+</template>
---
+++ src/templates/WalletView.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import ConnectWallet from "@/components/ConnectWallet.vue";
+import WalletInfo from "@/components/WalletInfo.vue";
+</script>
+
+<template>
+  <div>
+    <ConnectWallet />
+    <div>----------------------</div>
+    <WalletInfo />
+  </div>
+</template>
