2e493189aaedfda46eb15a6e044b09c7a9d4134c
mac0923 <64899758+mac0923@users.noreply.github.com>
2025-12-02 00:23:03 +0800

    feat: dashboard data

--- src/types/heroPath.ts
+++ src/types/heroPath.ts
@@ -3,7 +3,7 @@ import { EvaluationStep1Config, EvaluationStep2Config } from './evaluation'
 export interface UserEvaluation {
   accountId: string
   evaluationConfig: EvaluationStep1Config | EvaluationStep2Config
-  accountType: 'allocation' | 'evaluation'
+  accountType: 'funded' | 'evaluation'
   accountStatus: 'active' | 'inactive'
   displayStatus: {
     showDrawdown: boolean
