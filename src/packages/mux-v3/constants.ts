4ea4da6c8193b122f95a769391b658e9f1265733
Tatiana <12190709+theory-of-soul@users.noreply.github.com>
2025-12-01 15:42:05 +0800

    fix: update prod settings

--- src/packages/mux-v3/constants.ts
+++ src/packages/mux-v3/constants.ts
@@ -8,8 +8,8 @@ export const MUX_V3_ORACLE_API = import.meta.env.PROD ? `${window.location.origi
 export const MUX3_MQTT_SERVICE = import.meta.env.PROD ? `${window.location.host}` : 'app.mux.network'
 export const MUX3_MQTT_SERVICE_PORT = 443
 export const MUX3_MQTT_SERVICE_PATH = import.meta.env.PROD ? '/api/oracle' : '/api/mux3/oracle'
-export const MUX3_MQTT_SERVICE_USERNAME = 'mux3oracle'
-export const MUX3_MQTT_SERVICE_PASSWORD = 'mux3oracle'
+export const MUX3_MQTT_SERVICE_USERNAME = import.meta.env.PROD ? 'muxoracle' : 'mux3oracle'
+export const MUX3_MQTT_SERVICE_PASSWORD = import.meta.env.PROD ? 'muxoracle' : 'mux3oracle'
 export const MUX3_MQTT_SERVICE_USE_SSL = true
 
 export const MUX_V3_SUPPORTED_MARKETS = new Set<string>(['BTC/USD', 'ETH/USD']) 
