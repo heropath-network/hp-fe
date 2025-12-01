3755f7951bfd785b1092323270228611151c5031
Tatiana <phoenix-12@ya.ru>
2025-12-01 14:41:28 +0800

    preparing for prod build

--- src/packages/mux-v3/constants.ts
+++ src/packages/mux-v3/constants.ts
@@ -2,22 +2,15 @@
  * MUX V3 Oracle Configuration
  */
 
-export const MUX_V3_ORACLE_API = import.meta.env.PROD
-  ? "https://dev1.heropath.network/api"
-  : "https://app.mux.network/api";
+export const MUX_V3_ORACLE_API = import.meta.env.PROD ? `${window.location.origin}/api` : 'https://app.mux.network/api'
 
 // MQTT Configuration
-export const MUX3_MQTT_SERVICE = "app.mux.network";
-export const MUX3_MQTT_SERVICE_PORT = 443;
-export const MUX3_MQTT_SERVICE_PATH = "/api/mux3/oracle";
-export const MUX3_MQTT_SERVICE_USERNAME = "mux3oracle";
-export const MUX3_MQTT_SERVICE_PASSWORD = "mux3oracle";
-export const MUX3_MQTT_SERVICE_USE_SSL = true;
+export const MUX3_MQTT_SERVICE = import.meta.env.PROD ? `${window.location.host}` : 'app.mux.network'
+export const MUX3_MQTT_SERVICE_PORT = 443
+export const MUX3_MQTT_SERVICE_PATH = import.meta.env.PROD ? '/api/oracle' : '/api/mux3/oracle'
+export const MUX3_MQTT_SERVICE_USERNAME = 'mux3oracle'
+export const MUX3_MQTT_SERVICE_PASSWORD = 'mux3oracle'
+export const MUX3_MQTT_SERVICE_USE_SSL = true
 
-
-export const MUX_V3_SUPPORTED_MARKETS = new Set<string>([
-  'BTC/USD',
-  'ETH/USD',
-  'WLFI/USD',
-]); 
+export const MUX_V3_SUPPORTED_MARKETS = new Set<string>(['BTC/USD', 'ETH/USD']) 
 
