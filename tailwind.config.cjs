a2debb402c80db3462a14c14ee8a5f75fe26cd93
Tatiana <12190709+theory-of-soul@users.noreply.github.com>
2025-12-02 13:31:13 +0800

    feat: add Tailwind CSS configuration and PostCSS setup

---
+++ tailwind.config.cjs
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+      extend: {
+        colors: {
+          gray: {
+            950: '#0a0e1a',
+            900: '#0f1529',
+            800: '#1D1D1D',
+            700: '#272727',
+            600: '#4b5563',
+            500: '#6b7280',
+            400: '#9B9B9B',
+            300: '#d1d5db',
+          },
+          green: {
+            success: '#10C8A8',
+          },
+          red: {
+            error: '#FF4E59',
+          },
+        },
+      },
+    },
+    plugins: [],
+  }
+  
