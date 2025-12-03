5c3e8854dc4c6de6769d7e91809cdc7f5b2c092d
Tatiana <12190709+theory-of-soul@users.noreply.github.com>
2025-12-03 16:37:42 +0800

    refactor: update MarketSelect component with improved favorite icon handling and sorting logic

--- src/utils/favorites.ts
+++ src/utils/favorites.ts
@@ -1,7 +1,7 @@
 import { useLocalStorage } from '@vueuse/core'
 
 const FAVORITES_STORAGE_KEY = 'market-favorites'
-const DEFAULT_FAVORITES: string[] = ['BTC/USD', 'ETH/USD', 'ALIF/USD']
+const DEFAULT_FAVORITES: string[] = ['BTC/USD','BNB/USD',  'GG']
 
 export const favoritesStorage = useLocalStorage<string[]>(FAVORITES_STORAGE_KEY, DEFAULT_FAVORITES)
 
