3e7a62640d1a29046b38f9338e213383b79e7a41
mac0923 <64899758+mac0923@users.noreply.github.com>
2025-12-04 13:48:34 +0800

    chore: add logo

--- src/views/Header/Header.vue
+++ src/views/Header/Header.vue
@@ -2,6 +2,7 @@
 import { useRouter } from 'vue-router'
 import ConnectWalletButton from './ConnectWalletButton.vue'
 import { ROUTE_NAMES } from '@/router'
+import LogoIcon from '@/assets/HeroPath.svg'
 
 const router = useRouter()
 
@@ -15,7 +16,7 @@ function goNewEvaluation() {
     class="fixed inset-x-0 top-0 z-30 flex h-16 items-center justify-between border-b border-[var(--hp-line-color)] bg-[var(--hp-bg-dark)] px-6"
   >
     <div class="flex items-center gap-3 text-sm">
-      <span class="h-6 w-6 rounded-sm bg-[var(--hp-primary-green)]"> </span>
+      <img :src="LogoIcon" alt="" class="drop select-none" draggable="false" @dragstart.prevent />
     </div>
     <div class="flex items-center gap-4 text-sm text-[var(--hp-text-color)]">
       <button
