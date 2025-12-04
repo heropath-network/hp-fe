4e52f4a46ff13c9ad7831fcb7e36e8bec7ac5997
Tatiana <phoenix-12@ya.ru>
2025-12-04 19:38:21 +0800

    feat: enhance MarketIcon component to support meme market icons and update FOUR_MEME_MARKETS with new data

--- src/utils/favorites.ts
+++ src/utils/favorites.ts
@@ -1,7 +1,7 @@
 import { useLocalStorage } from '@vueuse/core'
 
 const FAVORITES_STORAGE_KEY = 'market-favorites'
-const DEFAULT_FAVORITES: string[] = ['BTC/USD','BNB/USD',  'GG']
+const DEFAULT_FAVORITES: string[] = ['BTC/USD', 'BNB/USD', 'GG', 'PALU']
 
 export const favoritesStorage = useLocalStorage<string[]>(FAVORITES_STORAGE_KEY, DEFAULT_FAVORITES)
 
